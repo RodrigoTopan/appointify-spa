614f6be88b0cd8878cbbb0774488b18aa630d997
Roberta Bononi da Silva <roberta.silva@ibm.com>
2023-02-03 23:10:35 -0300

    feat(AppComponent): definição de rota login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: 'login', component: LoginComponent },
+	{ path: '',   redirectTo: '/login', pathMatch: 'full' }
+  // { path: 'second-component', component: SecondComponent },
+  // { path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
